81a220a5b59a0f336dec687987b4471759598e83
Shreyas2004wagh <shreyaswagh2004@gmail.com>
2024-01-24 15:54:27 +0530

    done

---
+++ app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Form from './Components/Form'
+ import React from 'react'
+ 
+ const App = () => {
+   return (
+     <>
+     <Form/>
+     </>
+   )
+ }
+ 
+ export default App
+ 
